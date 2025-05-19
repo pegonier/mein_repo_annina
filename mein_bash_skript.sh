8c347d15363d766a73827ceca352026a1bd7f8ba
anngae <annina.gaehwiler@zhdk.ch>
2025-05-19 15:31:08 +0200

    Initial commit

---
+++ mein_bash_skript.sh
@@ -0,0 +1,9 @@
+# Mit diesem Skript teste ich erstmals nano bash in der Shell. 
+echo "Hallo zusammen. das ist der Start des Skripts."
+
+for FILE in *txt
+do
+echo "$FILE"
+done
+
+echo "Skript beendet.ENDE""
